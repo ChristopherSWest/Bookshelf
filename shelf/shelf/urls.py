5eb0e7ddc6f36a879c4a769af825eec804797b1f
cwest1 <christopher.west@benefitfocus.com>
2021-03-10 22:14:33 -0500

    added login and logout functionality

--- shelf/shelf/urls.py
+++ shelf/shelf/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
     path('books/', include("Books.urls"))
 ]
